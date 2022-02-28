b842260f4a1bfe2ecb6fddad345ed38e3264ffeb
itt828 <iittt828@gmail.com>
2022-03-01 00:51:32 +0900

    :construction: add requests.rs

---
+++ src/requests.rs
@@ -0,0 +1,34 @@
+use hyper::{Body, Client, Method, Request};
+use hyper_tls::HttpsConnector;
+use serde_json::json;
+use std::env;
+
+pub async fn post_to_traq(text: String, channel_id: String) {
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(format!(
+            "https://q.trap.jp/api/v3/channels/{}/messages",
+            channel_id
+        ))
+        .header("Content-Type", "application/json")
+        .header(
+            "Authorization",
+            format!(
+                "Bearer {}",
+                env::var("BOT_ACCESS_TOKEN").expect("BOT_ACCESS_TOKEN not found")
+            ),
+        )
+        .body(Body::from(
+            json!({
+              "content": text,
+              "embed": true
+            })
+            .to_string(),
+        ))
+        .unwrap();
+    println!("{:?}", req);
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, Body>(https);
+    let resp = client.request(req).await.unwrap();
+    println!("Response: {}", resp.status());
+}
