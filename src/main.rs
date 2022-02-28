171c8b0578ac73a42d705da9db44036fc3e2659e
itt828 <iittt828@gmail.com>
2022-03-01 00:50:15 +0900

    :recycle: refact main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod actions;
+mod requests;
+mod utils;
+use actions::*;
 use axum::{extract::Json, routing::any, Router};
 use http::{HeaderMap, StatusCode};
-use hyper::{Body, Client, Method, Request};
-use hyper_tls::HttpsConnector;
-use serde_json::{json, Value};
+use serde_json::Value;
 use std::env;
 use std::net::SocketAddr;
 
@@ -30,70 +32,16 @@ async fn handler(body: Json<Value>, headers: HeaderMap) -> StatusCode {
         return StatusCode::BAD_REQUEST;
     }
     let event = event.unwrap();
-    if is_token_valid(token) {
+    if token == env::var("BOT_VERIFICATION_TOKEN").expect("BOT_VERIFICATION_TOKEN not found") {
         return handle_event(event, body).await;
     } else {
         return StatusCode::UNAUTHORIZED;
     }
 }
 async fn handle_event(event: &str, body: Json<Value>) -> StatusCode {
-    if event == "PING" {
-        println!("PING");
-        return StatusCode::NO_CONTENT;
-    } else if event == "MESSAGE_CREATED" {
-        handle_message_created(body).await;
-        return StatusCode::NO_CONTENT;
+    match event {
+        "PING" => handle_ping(body),
+        "MESSAGE_CREATED" => handle_message_created(body).await,
+        _ => StatusCode::NOT_IMPLEMENTED,
     }
-    StatusCode::NOT_IMPLEMENTED
-}
-
-async fn handle_message_created(body: Json<Value>) {
-    let text = body["message"]["plainText"].to_string();
-    let text = remove_double_quotes(text);
-    let channel_id = body["message"]["channelId"].to_string();
-    let channel_id = remove_double_quotes(channel_id);
-    println!("text = {}, channel_id = {}", text, channel_id);
-    if body["message"]["user"]["bot"] == true {
-        println!("message from bot");
-    } else {
-        post_to_traq(text, channel_id).await;
-    }
-}
-fn remove_double_quotes(string: String) -> String {
-    let le = string.len();
-    string[1..le - 1].to_string()
-}
-
-fn is_token_valid(token: String) -> bool {
-    return token == env::var("BOT_VERIFICATION_TOKEN").expect("BOT_VERIFICATION_TOKEN not found");
-}
-
-async fn post_to_traq(text: String, channel_id: String) {
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri(format!(
-            "https://q.trap.jp/api/v3/channels/{}/messages",
-            channel_id
-        ))
-        .header("Content-Type", "application/json")
-        .header(
-            "Authorization",
-            format!(
-                "Bearer {}",
-                env::var("BOT_ACCESS_TOKEN").expect("BOT_ACCESS_TOKEN not found")
-            ),
-        )
-        .body(Body::from(
-            json!({
-              "content": text,
-              "embed": true
-            })
-            .to_string(),
-        ))
-        .unwrap();
-    println!("{:?}", req);
-    let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, Body>(https);
-    let resp = client.request(req).await.unwrap();
-    println!("Response: {}", resp.status());
 }
