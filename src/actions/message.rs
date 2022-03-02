c87f167354be306b81f3247ca9b354ef59afdd1f
itt828 <iittt828@gmail.com>
2022-03-02 09:15:48 +0900

    :construction: feat post_stamp

--- src/actions/message.rs
+++ src/actions/message.rs
@@ -1,5 +1,5 @@
 use crate::models::events::message::*;
-use crate::requests::post_message;
+use crate::requests::{post_message, post_stamp};
 use http::StatusCode;
 
 pub async fn handle_message_created(body: MessageCreated) -> StatusCode {
@@ -14,5 +14,11 @@ pub async fn handle_message_created(body: MessageCreated) -> StatusCode {
     if !body.message.user.bot {
         post_message(body.message.plain_text, body.message.channel_id).await;
     }
+    post_stamp(
+        body.message.id,
+        "e5849def-9b32-4050-a0c3-e6b73c83a822".to_string(),
+        10,
+    )
+    .await;
     StatusCode::NO_CONTENT
 }
