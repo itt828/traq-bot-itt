bee7d44d3fcc317eda7907b4a7b260eff1a1c61e
itt828 <iittt828@gmail.com>
2022-03-02 09:05:17 +0900

    :zap: add post_stamp

--- src/requests.rs
+++ src/requests.rs
@@ -1,4 +1,4 @@
-use hyper::{Body, Client, Method, Request};
+use hyper::{header, Body, Client, Method, Request};
 use hyper_tls::HttpsConnector;
 use serde_json::json;
 use std::env;
@@ -36,21 +36,39 @@ pub async fn post_message(content: String, channel_id: String) {
     println!("Response: {:?}", resp);
     println!("-------\n");
 }
-                "Bearer {}",
-                env::var("BOT_ACCESS_TOKEN").expect("BOT_ACCESS_TOKEN not found")
-            ),
-        )
-        .body(Body::from(
-            json!({
-              "content": content,
-              "embed": true
-            })
-            .to_string(),
-        ))
+
+pub async fn post_stamp(message_id: String, stamp_id: String, count: u64) {
+    let url = format!(
+        "https://q.trap.jp/api/v3/{}/stamps/{}",
+        message_id, stamp_id
+    );
+    let auth_value = format!(
+        "Bearer {}",
+        env::var("BOT_ACCESS_TOKEN").expect("BOT_ACCESS_TOKEN not found")
+    );
+
+    let body_content = Body::from(
+        json!({
+          "count": count,
+        })
+        .to_string(),
+    );
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(url)
+        .header("Content-Type", "application/json")
+        .header("Authorization", auth_value)
+        .body(body_content)
         .unwrap();
-    println!("{:?}", req);
+
+    println!("---post stamp---");
+    println!("Request: {:?}", req);
+
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, Body>(https);
     let resp = client.request(req).await.unwrap();
-    println!("Response: {}", resp.status());
+
+    println!("Response: {:?}", resp);
+    println!("-------\n");
 }
