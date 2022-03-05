dfc12ed0421c7d7c85e22b2b8654f2297d8ac8a4
itt828 <iittt828@gmail.com>
2022-03-05 09:55:41 +0900

    Revert ":sparkles: quote kinanogacha"
    This reverts commit
    bccdfaebb46173d262762ca0ff2a83c36808745e.

--- src/actions/message.rs
+++ src/actions/message.rs
@@ -1,8 +1,7 @@
 use crate::models::events::message::*;
-use crate::patterns::is_gacha;
 use crate::requests::{post_message, post_stamp};
-use crate::utils::*;
 use http::StatusCode;
+
 pub async fn handle_message_created(body: MessageCreated) -> StatusCode {
     println!(
         "[{}] MESSAGE_CREATED from: {}, content: {}, channel_id: {}, bot: {}",
@@ -13,16 +12,9 @@ pub async fn handle_message_created(body: MessageCreated) -> StatusCode {
         body.message.user.bot,
     );
     if !body.message.user.bot {
-        if is_gacha(body.message.plain_text) {
-            let content = format!(
-                ":nige::nige_dot: https//https://q.trap.jp/messages/{}",
-                body.message.id
-            );
-            post_message(content, body.message.channel_id).await;
+        post_message(body.message.plain_text, body.message.channel_id).await;
     }
-    }
-    StatusCode::NO_CONTENT
-}
+    post_stamp(
         body.message.id,
         "e5849def-9b32-4050-a0c3-e6b73c83a822".to_string(),
         10,
