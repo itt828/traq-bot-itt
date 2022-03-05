1f5ba9aff4e6b62f8e1e0794b9713e6fe3f16dba
itt828 <iittt828@gmail.com>
2022-03-05 09:37:46 +0900

    :bug: fix regex

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
 pub fn is_gacha(text: String) -> bool {
-    let re = Regex::new(r"kinanogacha").unwrap();
+    let re = Regex::new(r"catgacha").unwrap();
     re.is_match(&text)
 }
