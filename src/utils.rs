103d2fef59a960abf151449324ece2073ab6caa2
itt828 <iittt828@gmail.com>
2022-03-05 09:10:22 +0900

    :zap: add extract_message_id from url

--- src/utils.rs
+++ src/utils.rs
@@ -1,1 +1,20 @@
+use regex::Regex;
+pub fn extract_message_id(url: &str) -> Option<String> {
+    let re =
+        Regex::new(r"^https://q.trap.jp/messages/(?P<message_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{)$").unwrap();
+    if let Some(matches) = re.captures(url) {
+        let message_id = &matches["message_id"];
+        return Some(message_id.to_string());
+    }
+    None
+}
 
+#[test]
+fn extract_message_id_works() {
+    let url = r"https://q.trap.jp/messages/33a865ef-cae1-4f79-a65f-a02eb9d7ca48";
+    let s = extract_message_id(url);
+    assert_eq!(
+        s,
+        Some(String::from("33a865ef-cae1-4f79-a65f-a02eb9d7ca48"))
+    );
+}
