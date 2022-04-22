3342c65d7b3fd53b2e88e852a4b69551ea63e7c7
itt828 <iittt828>
2022-04-23 01:05:42 +0900

    :recycle: move extract_message_id

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,10 +1,29 @@
 use regex::Regex;
 
-pub fn is_gacha(text: String) -> bool {
+pub fn is_gacha(text: &str) -> bool {
     let re = Regex::new(r"catgacha").unwrap();
     if re.is_match(&text) {
         println!("is_gacha matched!");
         return true;
     }
     false
+}
+pub fn extract_message_id(url: &str) -> Option<String> {
+    let re =
+        Regex::new(r"^(https://q.trap.jp/messages/)?(?P<message_id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{)$").unwrap();
+    if let Some(matches) = re.captures(url) {
+        let message_id = &matches["message_id"];
+        return Some(message_id.to_string());
+    }
+    None
+}
+
+#[test]
+fn extract_message_id_works() {
+    let url = r"https://q.trap.jp/messages/33a865ef-cae1-4f79-a65f-a02eb9d7ca48";
+    let s = extract_message_id(url);
+    assert_eq!(
+        s,
+        Some(String::from("33a865ef-cae1-4f79-a65f-a02eb9d7ca48"))
+    );
 }
