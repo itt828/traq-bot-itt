bafaf65a5dc03cef8a312c96b774966bfd01d380
itt828 <iittt828>
2022-05-15 12:16:35 +0900

    :zap: add join

---
+++ traq/src/models/api/bot/join.rs
@@ -0,0 +1,26 @@
+use crate::bot::Bot;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Join {
+    pub channel_id: String,
+}
+
+impl Bot {
+    pub async fn join(
+        &self,
+        bot_id: &str,
+        channel_id: &str,
+    ) -> Result<Join, Box<dyn std::error::Error>> {
+        let url = format!("{}/bots/{}/actions/join", self.base_url, bot_id);
+        let body = json!({ "channelId": channel_id });
+        let resp = self
+            .api_request_base(&url, Method::POST, body)
+            .await?
+            .json::<Join>()
+            .await?;
+        Ok(resp)
+    }
+}
