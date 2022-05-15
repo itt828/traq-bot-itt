ee4d37bd6a8e45e14c5c43d5dc1c85a36aae1c1b
itt828 <iittt828>
2022-05-15 12:19:39 +0900

    :zap: add get_subscription

---
+++ traq/src/models/api/channel/get_subscription.rs
@@ -0,0 +1,25 @@
+use crate::bot::Bot;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct GetSubscription {
+    user_ids: Vec<String>,
+}
+
+impl Bot {
+    pub async fn get_subscription(
+        &self,
+        channel_id: &str,
+    ) -> Result<GetSubscription, Box<dyn std::error::Error>> {
+        let url = format!("{}/channels/{}/subscribers", self.base_url, channel_id);
+        let body = json!({});
+        let resp = self
+            .api_request_base(&url, Method::GET, body)
+            .await?
+            .json::<GetSubscription>()
+            .await?;
+        Ok(resp)
+    }
+}
