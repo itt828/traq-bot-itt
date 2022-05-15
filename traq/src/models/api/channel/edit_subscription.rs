7c06e8e5d76708a2a9fdba6c66e7a13e65361cf6
itt828 <iittt828>
2022-05-15 12:17:59 +0900

    :zap: add edit_subscription

---
+++ traq/src/models/api/channel/edit_subscription.rs
@@ -0,0 +1,25 @@
+use crate::bot::Bot;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EditSubscription {}
+
+impl Bot {
+    pub async fn edit_subscription(
+        &self,
+        channel_id: &str,
+        on_users: &Vec<String>,
+        off_users: &Vec<String>,
+    ) -> Result<EditSubscription, Box<dyn std::error::Error>> {
+        let url = format!("{}/channels/{}/subscribers", self.base_url, channel_id);
+        let body = json!({ "on": on_users,"off":off_users });
+        let resp = self
+            .api_request_base(&url, Method::PATCH, body)
+            .await?
+            .json::<EditSubscription>()
+            .await?;
+        Ok(resp)
+    }
+}
