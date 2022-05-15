c46528a493d7f9ec373456ed00fc59b20f52deaa
itt828 <iittt828>
2022-05-15 12:21:46 +0900

    :zap: add add_stamp

---
+++ traq/src/models/api/message/add_stamp.rs
@@ -0,0 +1,28 @@
+use crate::bot::Bot;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AddStamp {}
+
+impl Bot {
+    pub async fn add_stamp(
+        &self,
+        message_id: &str,
+        stamp_id: &str,
+        count: usize,
+    ) -> Result<AddStamp, Box<dyn std::error::Error>> {
+        let url = format!(
+            "{}/messages/{}/stamps/{}",
+            self.base_url, message_id, stamp_id
+        );
+        let body = json!({ "count": count });
+        let resp = self
+            .api_request_base(&url, Method::POST, body)
+            .await?
+            .json::<AddStamp>()
+            .await?;
+        Ok(resp)
+    }
+}
