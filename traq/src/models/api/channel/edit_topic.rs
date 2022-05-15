b0ba177ba8c49c0aef69cd9349b9ab67f348b6c0
itt828 <iittt828>
2022-05-15 12:18:32 +0900

    :zap: add edit_topic

---
+++ traq/src/models/api/channel/edit_topic.rs
@@ -0,0 +1,24 @@
+use crate::bot::Bot;
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EditTopic {}
+
+impl Bot {
+    pub async fn edit_topic(
+        &self,
+        channel_id: &str,
+        topic: &str,
+    ) -> Result<EditTopic, Box<dyn std::error::Error>> {
+        let url = format!("{}/channels/{}/topic", self.base_url, channel_id);
+        let body = json!({ "topic": topic });
+        let resp = self
+            .api_request_base(&url, Method::PUT, body)
+            .await?
+            .json::<EditTopic>()
+            .await?;
+        Ok(resp)
+    }
+}
