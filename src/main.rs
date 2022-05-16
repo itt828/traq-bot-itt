b90da618dd6ac257df8009f271d649530b5c24ae
itt828 <iittt828@gmail.com>
2022-05-17 00:19:00 +0900

    :zap: traq workspaceに合わせた

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 mod actions;
-mod apis;
 mod commands;
-mod earthquake;
-mod models;
 mod patterns;
-mod requests;
-mod utils;
-use crate::models::events::system::Ping;
-use actions::message::handle_message_created;
+use crate::actions::message::handle_message_created;
 use actions::system::handle_ping;
+use anyhow::Result;
 use axum::{extract::Json, routing::any, Router};
-use earthquake::earthquake;
 use http::{HeaderMap, StatusCode};
-use models::events::message::MessageCreated;
 use serde_json::{from_value, Value};
 use std::env;
 use std::net::SocketAddr;
+use traq::{bot::Bot, models::event::*};
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    earthquake().await?;
-
+async fn main() -> Result<()> {
+    let bot = Bot {
+        base_url: "".to_string(),
+        bot_access_token: "".to_string(),
+    };
+    let bot = std::sync::Arc::new(bot);
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    let app = Router::new().route("/", any(handler));
+    let app = Router::new().route("/", any(|body, headers| handler(bot, body, headers)));
     println!("serving at {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -30,7 +27,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn handler(body: Json<Value>, headers: HeaderMap) -> StatusCode {
+async fn handler(bot: std::sync::Arc<Bot>, body: Json<Value>, headers: HeaderMap) -> StatusCode {
     let token = headers["X-TRAQ-BOT-TOKEN"].to_str();
     if token.is_err() {
         println!("No X-TRAQ-BOT-TOKEN");
@@ -44,17 +41,24 @@ async fn handler(body: Json<Value>, headers: HeaderMap) -> StatusCode {
     }
     let event = event.unwrap();
     if token == env::var("BOT_VERIFICATION_TOKEN").expect("BOT_VERIFICATION_TOKEN not found") {
-        return handle_event(event, body).await;
+        return handle_event(bot, event, body).await;
     } else {
         return StatusCode::UNAUTHORIZED;
     }
 }
-async fn handle_event(event: &str, Json(body): Json<Value>) -> StatusCode {
+async fn handle_event(
+    bot: std::sync::Arc<Bot>,
+    event: &str,
+    Json(body): Json<Value>,
+) -> StatusCode {
     match event {
-        "PING" => handle_ping(from_value::<Ping>(body).unwrap()),
-        "MESSAGE_CREATED" | "DIRECT_MESSAGE_CREATED" => {
-            handle_message_created(from_value::<MessageCreated>(body).unwrap()).await
-        }
+        "PING" => handle_ping(from_value::<system::Ping>(body).unwrap()),
+        "MESSAGE_CREATED" | "DIRECT_MESSAGE_CREATED" => handle_message_created(
+            bot,
+            std::sync::Arc::new(from_value::<message::MessageCreated>(body).unwrap()),
+        )
+        .await
+        .unwrap(),
         _ => StatusCode::NOT_IMPLEMENTED,
     }
 }
