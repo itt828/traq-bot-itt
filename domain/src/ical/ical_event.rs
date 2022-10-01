2a31b3ad39aa1b06ae9c9416efc67ad2715ae80b
itt828 <iittt828@gmail.com>
2022-10-01 19:26:30 +0900

    :zap: fix ical-event domain

--- domain/src/ical/ical_event.rs
+++ domain/src/ical/ical_event.rs
@@ -1,9 +1,18 @@
 use chrono::{DateTime, Local};
 #[derive(Debug)]
 pub struct IcalEvent {
-    pub title: String,
-    pub start_time: DateTime<Local>,
-    pub end_time: DateTime<Local>,
-    pub calendar_name: String,
+    /// イベントが開始される時間
+    pub start_dt: DateTime<Local>,
+    /// イベントが終了する時間
+    pub end_dt: DateTime<Local>,
+    /// イベントのタイトル
     pub summary: String,
+    /// イベントの説明・本文
+    pub description: Option<String>,
+    /// イベントの一意なID
+    pub uid: Option<String>,
+    /// イベントの開催場所
+    pub location: Option<String>,
+    /// イベントの繰り返しのルール
+    pub rrule: Option<String>,
 }
