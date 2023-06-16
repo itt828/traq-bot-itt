a53521045004c709245c7ed332d9fd669c28214f
itt828 <iittt828@gmail.com>
2023-06-16 20:24:52 +0900

    :fire: Remove code

--- src/actions/system.rs
+++
@@ -1,7 +0,0 @@
-use http::StatusCode;
-use traq::models::event::system::*;
-
-pub fn handle_ping(body: Ping) -> StatusCode {
-    println!("[{}] PING", body.event_time);
-    StatusCode::NO_CONTENT
-}
