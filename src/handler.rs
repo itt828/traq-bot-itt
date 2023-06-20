0fc11ca400cbb7f0200acf453d33d5afb558acad
itt828 <iittt828@gmail.com>
2023-06-21 00:27:31 +0900

    :zap: Add unwrap

--- src/handler.rs
+++ src/handler.rs
@@ -2,12 +2,19 @@ use crate::{command::exec_command, Resource};
 use std::sync::Arc;
 use traq_ws_bot::events::payload;
 
-pub async fn message_created_handler(payload: payload::MessageCreated, resource: Arc<Resource>) {
-    exec_command(payload.message.clone(), resource).await;
+pub async fn message_created_handler(
+    payload: payload::MessageCreated,
+    resource: Arc<Resource>,
+) -> () {
+    exec_command(payload.message.clone(), resource)
+        .await
+        .unwrap();
 }
 pub async fn direct_message_created_handler(
     payload: payload::DirectMessageCreated,
     resource: Arc<Resource>,
-) {
-    exec_command(payload.message.clone(), resource).await;
+) -> () {
+    exec_command(payload.message.clone(), resource)
+        .await
+        .unwrap();
 }
