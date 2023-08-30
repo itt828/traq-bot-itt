b0c608ebf25ecea1887a09ba03fa64b47baa9974
itt828 <iittt828@gmail.com>
2023-08-30 12:44:47 +0900

    :zap: Pass image to tesseract

--- src/command/poke_recipe.rs
+++ src/command/poke_recipe.rs
@@ -1,9 +1,19 @@
-use std::{str::FromStr, sync::Arc};
+use std::{
+    ffi::OsString,
+    io::{Read, Write},
+    os::unix::prelude::OsStringExt,
+    process::{Command, Stdio},
+    str::FromStr,
+    sync::Arc,
+};
 
 use clap::Args;
 use regex::Regex;
+use reqwest::{Client, Method};
 use traq::{
-    apis::{bot_api::let_bot_join_channel, channel_api::post_message},
+    apis::{
+        bot_api::let_bot_join_channel, channel_api::post_message, configuration::Configuration,
+    },
     models::{PostBotActionJoinRequest, PostMessageRequest},
 };
 use traq_ws_bot::events::common::Message;
@@ -21,29 +31,55 @@ pub async fn handle_poke_recipe(
     message: Message,
     resource: Arc<Resource>,
 ) -> anyhow::Result<()> {
-    let _ = post_message(
+    let file_id = extract_file_id(message.text)?;
+    let image = get_traq_image_binary(&resource.configuration, file_id)
+        .await
+        .expect("hoge");
+
+    let ocr_text: String = {
+        let process = Command::new("tesseract")
+            .arg("-")
+            .arg("stdout")
+            .arg("-l")
+            .arg("jpn")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::null())
+            .spawn()?;
+        process.stdin.unwrap().write_all(&image)?;
+        let mut s = String::new();
+        process.stdout.unwrap().read_to_string(&mut s)?;
+        s
+    };
+    post_message(
         &resource.configuration,
         &message.channel_id,
         Some(PostMessageRequest {
-            content: extract_file_id(message.text)?,
+            content: ocr_text,
             embed: None,
         }),
     )
-    .await;
-    let _ = let_bot_join_channel(
-        &resource.configuration,
-        &resource.bot_id,
-        Some(PostBotActionJoinRequest {
-            channel_id: uuid::Uuid::from_str(&message.channel_id)?,
-        }),
-    )
-    .await;
+    .await?;
     Ok(())
 }
 
 pub fn extract_file_id(text: String) -> anyhow::Result<String> {
     let regex = Regex::new("https://q.trap.jp/files/(?<id>([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{12}))")?;
     let caps = regex.captures(&text);
-    println!("{:?}", caps);
     Ok(caps.unwrap()["id"].to_string())
 }
+
+pub async fn get_traq_image_binary(config: &Configuration, id: String) -> anyhow::Result<Vec<u8>> {
+    let auth_value = format!("Bearer {}", config.bearer_access_token.clone().unwrap());
+    let client = Client::new();
+    let req = client
+        .request(
+            Method::GET,
+            format!("https://q.trap.jp/api/v3/files/{}", id),
+        )
+        .header("Authorization", auth_value);
+    let bin = req.send().await?.bytes().await.expect("oi").to_vec();
+    Ok(bin)
+}
+
+// pub fn
